7544f4aac5d233b0e1193b467ebb833130e26cec
Koczkás Péter <petikoczkas@hotmail.com>
2023-02-25 15:25:51 +0100

    Create the base of the ListScreen.kt

--- app/src/main/java/hu/codeyard/android/codeyard/MainActivity.kt
+++ app/src/main/java/hu/codeyard/android/codeyard/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import hu.codeyard.android.codeyard.ui.theme.CodeYardTheme
+import hu.codeyard.android.codeyard.ui.view.AppBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +20,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodeYardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                /*Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
-                }
+                }*/
+                Scaffold(
+                    topBar = { AppBar() },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Rounded.Add, contentDescription = null)
+                        }
+                    },
+                    content = { padding ->
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(padding)
+                        ) {
+                            Greeting(name = "Android")
+                        }
+                    }
+                )
             }
         }
     }
