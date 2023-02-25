048a27db37355b0a384b58fde22b27edb87c541d
Koczkás Péter <petikoczkas@hotmail.com>
2023-02-25 11:50:22 +0100

    Update CodeYardTheme

--- app/src/main/java/hu/codeyard/android/codeyard/ui/theme/Color.kt
+++ app/src/main/java/hu/codeyard/android/codeyard/ui/theme/Color.kt
@@ -1,8 +1,32 @@
 package hu.codeyard.android.codeyard.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+data class CodeYardColors(
+    val white: Color = Color(0xFFFFFFFF),
+    val black: Color = Color(0xFF000000),
+    val gray: Color = Color.Gray,
+
+    val blue: Color = Color(0xFF2A56C6),
+    val grayBlue: Color = Color(0xFF607D8B),
+
+    val colors: Colors = lightColors(
+        primary = blue,
+        onPrimary = black,
+        secondary = grayBlue,
+        onSecondary = black
+    )
+)
+
+val LocalColors = staticCompositionLocalOf { CodeYardColors() }
+
+val MaterialTheme.codeyardColors: CodeYardColors
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalColors.current
--- app/src/main/java/hu/codeyard/android/codeyard/ui/theme/Type.kt
+++ app/src/main/java/hu/codeyard/android/codeyard/ui/theme/Type.kt
@@ -1,28 +1,33 @@
 package hu.codeyard.android.codeyard.ui.theme
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
+
+data class CodeYardTypography(
+    val cardTitleTextStyle: TextStyle = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+)
+
+val LocalTypography = staticCompositionLocalOf { CodeYardTypography() }
+
+val MaterialTheme.cocktailTypography: CodeYardTypography
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalTypography.current
